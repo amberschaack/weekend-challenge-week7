c97095ef04fc04bc6efe554ff51b2cffb6215049
Amber Schaack <aschaack9@gmail.com>
2024-05-03 16:58:46 -0500

    addEmployee function adds up salary, delete recalculates monthly salary

--- client.js
+++ client.js
@@ -4,30 +4,62 @@ function onReady() {
   console.log('Javascript is running!');
 }
 
+let employees = [];
+
 function addEmployee(event) {
   event.preventDefault();
-  //   const submitButton = document.getElementById('submit-button');
-  //   submitButton = event.target;
-  console.log('Submit');
-  const table = document.getElementById('table-body');
+
   const firstName = document.getElementById('first-name').value;
   const lastName = document.getElementById('last-name').value;
   const ID = document.getElementById('i-d').value;
   const title = document.getElementById('title').value;
-  const annualSalary = document.getElementById('annual-salary').value;
-  table.innerHTML += `
-  <tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td>${ID}</td>
-    <td>${title}</td>
-    <td>${annualSalary}</td>
-    <td><button onclick="removeEmployee(event)">Delete</button></td>
-  </tr>
-  `;
+  const annualSalary = Number(document.getElementById('annual-salary').value);
+
+  const newEmployee = {
+    firstName,
+    lastName,
+    ID,
+    title,
+    annualSalary,
+  };
+
+  employees.push(newEmployee);
+
+  renderEmployees();
+  calculateMonthlySalary();
   document.getElementById('salary-form').reset();
 }
 
+function renderEmployees() {
+  const table = document.getElementById('table-body');
+  table.innerHTML = '';
+  for (let i = 0; i < employees.length; i++) {
+    table.innerHTML += `
+    <tr>
+      <td>${employees[i].firstName}</td>
+      <td>${employees[i].lastName}</td>
+      <td>${employees[i].ID}</td>
+      <td>${employees[i].title}</td>
+      <td>${employees[i].annualSalary}</td>
+      <td><button id=${i} onclick="removeEmployee(event)">Delete</button></td>
+     </tr>
+    `;
+  }
+}
+
 function removeEmployee(event) {
-  event.target.closest('tr').remove();
+  const removeButton = event.target.id;
+  employees.splice(Number(removeButton), 1);
+  renderEmployees();
+  calculateMonthlySalary();
+}
+
+function calculateMonthlySalary() {
+  let total = 0;
+  for (const employee of employees) {
+    total += employee.annualSalary / 12;
+  }
+  console.log(total);
+  const monthlyElement = document.getElementById('total-monthly');
+  monthlyElement.innerText = `Total Monthly: $${total}`;
 }
